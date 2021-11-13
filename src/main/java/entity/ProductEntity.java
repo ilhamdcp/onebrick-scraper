3608bd209f2a26868d02de56bc8edadbf2b91f62
Ilham Darmawan Candra Purnama <ilham.purnama@tokopedia.com>
2021-11-13 15:55:08 +0700

    create enum to specify product type to be searched and product POJO class

---
+++ src/main/java/entity/ProductEntity.java
@@ -0,0 +1,43 @@
+package entity;
+
+public class ProductEntity {
+    private String productName;
+    private String imageUrl;
+    private int price;
+    private String storeName;
+
+    public ProductEntity() {
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+}
